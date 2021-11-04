2a6bf50265c94138b7144243b5ba0e57a9afcbf4
Aidan MacDonald <amachronic@protonmail.com>
2021-11-04 15:56:27 +0000

    Restyle the source code
    This is frivolous, but I really don't like the current style.

--- src/microtar-stdio.c
+++ src/microtar-stdio.c
@@ -21,61 +21,65 @@
  * IN THE SOFTWARE.
  */
 
+#include "microtar.h"
 #include <stdio.h>
 #include <string.h>
 
-#include "microtar.h"
-
-static int file_write(mtar_t *tar, const void *data, unsigned size) {
-  unsigned res = fwrite(data, 1, size, tar->stream);
-  return (res == size) ? MTAR_ESUCCESS : MTAR_EWRITEFAIL;
+static int file_write(mtar_t* tar, const void* data, unsigned size)
+{
+    unsigned res = fwrite(data, 1, size, tar->stream);
+    return (res == size) ? MTAR_ESUCCESS : MTAR_EWRITEFAIL;
 }
 
-static int file_read(mtar_t *tar, void *data, unsigned size) {
-  unsigned res = fread(data, 1, size, tar->stream);
-  return (res == size) ? MTAR_ESUCCESS : MTAR_EREADFAIL;
+static int file_read(mtar_t* tar, void* data, unsigned size)
+{
+    unsigned res = fread(data, 1, size, tar->stream);
+    return (res == size) ? MTAR_ESUCCESS : MTAR_EREADFAIL;
 }
 
-static int file_seek(mtar_t *tar, unsigned offset) {
-  int res = fseek(tar->stream, offset, SEEK_SET);
-  return (res == 0) ? MTAR_ESUCCESS : MTAR_ESEEKFAIL;
+static int file_seek(mtar_t* tar, unsigned offset)
+{
+    int res = fseek(tar->stream, offset, SEEK_SET);
+    return (res == 0) ? MTAR_ESUCCESS : MTAR_ESEEKFAIL;
 }
 
-static int file_close(mtar_t *tar) {
-  fclose(tar->stream);
-  return MTAR_ESUCCESS;
+static int file_close(mtar_t* tar)
+{
+    fclose(tar->stream);
+    return MTAR_ESUCCESS;
 }
 
+int mtar_open(mtar_t* tar, const char* filename, const char* mode)
+{
+    int err;
+    mtar_header_t h;
 
-int mtar_open(mtar_t *tar, const char *filename, const char *mode) {
-  int err;
-  mtar_header_t h;
+    /* Init tar struct and functions */
+    memset(tar, 0, sizeof(*tar));
+    tar->write = file_write;
+    tar->read = file_read;
+    tar->seek = file_seek;
+    tar->close = file_close;
 
-  /* Init tar struct and functions */
-  memset(tar, 0, sizeof(*tar));
-  tar->write = file_write;
-  tar->read = file_read;
-  tar->seek = file_seek;
-  tar->close = file_close;
+    /* Assure mode is always binary */
+    if(strchr(mode, 'r')) mode = "rb";
+    if(strchr(mode, 'w')) mode = "wb";
+    if(strchr(mode, 'a')) mode = "ab";
 
-  /* Assure mode is always binary */
-  if ( strchr(mode, 'r') ) mode = "rb";
-  if ( strchr(mode, 'w') ) mode = "wb";
-  if ( strchr(mode, 'a') ) mode = "ab";
-  /* Open file */
-  tar->stream = fopen(filename, mode);
-  if (!tar->stream) {
-    return MTAR_EOPENFAIL;
-  }
-  /* Read first header to check it is valid if mode is `r` */
-  if (*mode == 'r') {
-    err = mtar_read_header(tar, &h);
-    if (err != MTAR_ESUCCESS) {
-      mtar_close(tar);
-      return err;
+    /* Open file */
+    tar->stream = fopen(filename, mode);
+    if(!tar->stream)
+        return MTAR_EOPENFAIL;
+
+    /* Read first header to check it is valid if mode is `r` */
+    if(*mode == 'r') {
+        err = mtar_read_header(tar, &h);
+        if(err != MTAR_ESUCCESS) {
+            mtar_close(tar);
+            return err;
+        }
     }
-  }
 
-  /* Return ok */
-  return MTAR_ESUCCESS;
+    /* Return ok */
+    return MTAR_ESUCCESS;
 }
